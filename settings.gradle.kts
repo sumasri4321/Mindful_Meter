dd6cf8a039baee51ed8c60f45b49ef7492f4dabf
sumasri4321 <152298478+sumasri4321@users.noreply.github.com>
2024-10-06 19:46:01 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+@file:Suppress("DEPRECATION")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // Add JitPack repository
+    }
+}
+
+rootProject.name = "MyPermission"
+include(":app")
